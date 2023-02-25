d9ecb0e7271666a78ab383fb272b663e11b13478
GiovanniCoppola084 <gac6151@rit.edu>
2023-02-25 13:00:27 -0500

    Added a function to test if the file being uploaded is an object file.

---
+++ web/FrontEndSim.js
@@ -0,0 +1,10 @@
+class FrontEndSim {
+    uploadFile() {
+        fileExtension = fileupload.split('.').pop();
+        if (fileExtension != 'o') {
+            window.alert("Please enter an object file.");
+        }
+
+        // Parse the file
+    }
+}
