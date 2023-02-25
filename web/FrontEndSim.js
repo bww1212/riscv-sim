fb8cff8608cf0798cab474bcbdd34111ac76b6e3
GiovanniCoppola084 <gac6151@rit.edu>
2023-02-25 14:06:49 -0500

    Function added to parse object file down to byte array to send to C++ code.

--- web/FrontEndSim.js
+++ web/FrontEndSim.js
@@ -1,10 +1,34 @@
 class FrontEndSim {
+    byteArrayFromObject;
+
     uploadFile() {
         fileExtension = fileupload.split('.').pop();
         if (fileExtension != 'o') {
             window.alert("Please enter an object file.");
+            return null;
         }
+    }
 
-        // Parse the file
+    parseFile(file) {
+        return new Promise((resolve, reject) => {
+            try {
+                var reader = new FileReader();
+                let byteArray = [];
+                reader.readAsArrayBuffer(file);
+                reader.onloadend = (evt) => {
+                    if (evt.target.readyState == FileReader.DONE) {
+                        let buffer = evt.target.result,
+                        array = new Uint8Array(buffer);
+                        for (byte of array) {
+                            byteArray.push(byte);
+                        }
+                    }
+                    resolve(byteArray);
+                }
+            }
+            catch (e) {
+                reject(e);
+            }
+        })
     }
 }
