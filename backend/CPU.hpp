3563af9f0ae243ad6090bb49f3b05b6d24ccd70d
mgk3715 <mgk3715@rit.edu>
2023-02-25 14:36:53 -0500

    Remove extra comma

--- backend/CPU.hpp
+++ backend/CPU.hpp
@@ -17,7 +17,7 @@ class CPU {
         uint8_t memory[4096];
         Register &zero, &ra, &sp, &gp, &tp, &fp;
         Register &t0, &t1, &t2, &t3, &t4, &t5, &t6;
-        Register &s0, &s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9, &s10, &s11,;
+        Register &s0, &s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9, &s10, &s11;
         Register &a0, &a1, &a2, &a3, &a4, &a5, &a6, &a7;
 };
 
