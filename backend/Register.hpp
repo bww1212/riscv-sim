52cbc53fb95729d9966f8a9745536822cc2d6b57
bww1212 <bww1212@rit.edu>
2023-02-25 13:04:10 -0500

    Partially implement Register

--- backend/Register.hpp
+++ backend/Register.hpp
@@ -4,13 +4,18 @@
 #include "Includes.hpp"
 
 class Register {
+	public:
 	Register(int bits);
 	~Register();
 	void send(Register o, int lsrc, int usrc, int tgt);
-	void send(Register o);
+	void send(Register o) { send(o, 0, this.nbits, 0); }
 	uint32_t operator()(int lower, int upper);
 	uint8_t  operator()(int bit);
 	uint32_t operator()(void);
+	
+	private:
+	int nbits;
+	uint8_t* data;
 }
 
 #endif
